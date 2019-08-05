b78e9174d3b601a6b2e558bb972ec47223967036
Levi Miller <paullevimiller@gmail.com>
2019-08-05 10:59:44 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Levi."
+  end
+  get '/hometown' do
+    "My hometown is Harrisonville."
+  end
+  get '/favorite-song' do
+    "My favorite song is Cleo."
+  end
 end
